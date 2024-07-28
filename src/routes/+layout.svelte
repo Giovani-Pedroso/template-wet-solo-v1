0af9b0b44e137a4501deb3120098737ea9814691
Giovani SFPV <giovanisfpv@gmail.com>
2024-07-27 21:06:15 -0300

    Home page done

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,9 @@
 	import '../app.css';
 	import Navbar from '../components/Navegation/Navbar/index.svelte';
 	import Footer from '../components/Navegation/Footer/index.svelte';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+	injectSpeedInsights();
 </script>
 
 <Navbar />
